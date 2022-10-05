742947b1e412f059e1fdb9428b3caaefc2e865e1
BrianZoulko <bzoulko@gmail.com>
2022-10-05 08:45:46 -0700

    Updated logic in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,42 @@
+/*
+    Created:    10/05/2022 
+    Programmer: Brian Zoulko
+    Notes:      Devopled JAVASCRIPT module was designed by bootcamp.
+
+    Modification
+    ============
+    10/05/2022 Brian Zoulko    Added necessary code for GET/PUT/POST/DELETE operations,
+                               updated all functions with async/await operations to ensure
+                               all calls operate in the order called.
+*/
+
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  
+  // 10/05/2022 BZ - Added logic to return all products.
+  const data = await Product.findAll();
+  return res.json(data);
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  
+  // 10/05/2022 BZ - Added logic to find one product by id.
+  const data = await Product.findByPk(req.params.id);
+  return res.json(data);
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,8 +45,16 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+
+  // 10/05/2022 BZ - Made sure the body contained all the necessary parts before
+  //                 creating the new entry. Also added async/await to processes.
+  if (!req.body.product_name) req.body.product_name = "Baseball";
+  if (!req.body.price)        req.body.price = 25.00;
+  if (!req.body.stock)        req.body.stock = 3;
+  if (!req.body.tagIds)       req.body.tagIds = [1, 2, 3, 4];
+
+  await Product.create(req.body)
+    .then(async (product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -35,7 +63,7 @@ router.post('/', (req, res) => {
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
+        return await ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
       res.status(200).json(product);
@@ -48,18 +76,18 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update product data
-  Product.update(req.body, {
+  await Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
-    .then((product) => {
+    .then(async (product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return await ProductTag.findAll({ where: { product_id: req.params.id } });
     })
-    .then((productTags) => {
+    .then(async (productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
@@ -89,8 +117,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+
+  // 10/05/2022 BZ - Add logic to remove one product at a time by id.
+  const data = await Product.destroy({
+    where: {
+        id: req.params.id 
+    }
+  })
+  if(data === 1) {
+      return res.json({ status: `Deleted product id = ${req.params.id}`});
+  } else {
+      return res.json({ status: `ERROR: Unable to delete product id = ${req.params.id}`});
+  }
+
 });
 
 module.exports = router;
